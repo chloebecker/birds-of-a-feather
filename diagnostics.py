9572bf058080a558f981cbf19f95d50b74d4ca66
Chloe Becker <chloe.a.becker@gmail.com>
2018-09-23 16:53:30 -0500

    fixed first test case --> can now test if droplet is up

--- diagnostics.py
+++ diagnostics.py
@@ -1,17 +1,33 @@
-
 import os
 
-hostname = "team12.softwareengineeringii.com"
-response = os.system("ping -c 1 " + hostname)
+dropletIp = "206.189.166.76"
 
-def pingTest():
+def dropletHealth():
+	print("\n-----------  BEGIN TEST CASE: dropletHealth()  -----------\n")
+	print("\nMaking request to " + dropletIp + "... ... ...")
+	print("------------------------------------------------------------\n")
+	## tidy -i formats output with indentation so it's easier to read
+	os.system("curl " + dropletIp + " | tidy -i")
+
+	## grep uses regex to find given text in the input file (in our case, the html for page we
+	## 		performed a get request on with cURL)
+	grepTxt = "\"<p>If you see this page, the nginx web server is successfully installed\""
+	print("\nSearching html with grep" + dropletIp + "... ... ...")
+	print("------------------------------------------------------------\n")
+
+	response = os.system("curl " + dropletIp + " | grep " + grepTxt)
+	
+	print("\nRESPONSE:")
+	print("------------------------------------------------------------\n")
 	if response == 0:
-	  print(hostname, 'is up!')
+		print(dropletIp + " is running!")
 	else:
-	  print(hostname, 'is down!')
+		print(dropletIp + " may be down, please check the droplet.")
+	print("\n-----------  END OF TEST CASE: dropletHealth()  -----------\n")
+	
 
 def runTests():
-	pingTest()
+	dropletHealth()
 
 
 runTests()
